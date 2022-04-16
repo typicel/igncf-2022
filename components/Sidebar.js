5da9533dda1326909c40bf7d5542bacc14483f05
Tyler McCormick <tyler.mccormick@colorado.edu>
2022-04-16 14:42:52 -0600

    Sidebar shows the 5 most recent polls created

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,19 +1,35 @@
 import React from "react";
 import Link from "next/link";
+import { Card } from "@mantine/core";
 
 const Sidebar = ({ polls }) => {
   return (
-    <div>
-      <h1> Recent Polls: </h1>
-      <ul>
+    <div className="sidebar">
+      <h1> Recent Polls </h1>
+      <div className="card-container">
         {polls.map((poll) => (
-          <li>
-            <Link href="/pollpage/[pollId]" as={`/pollpage/${poll.id}`}>
+          <Link href="/pollpage/[pollId]" as={`/pollpage/${poll.id}`}>
+            <Card shadow="sm">
               <a>{poll.title}</a>
-            </Link>
-          </li>
+            </Card>
+          </Link>
         ))}
-      </ul>
+      </div>
+      <style jsx>{`
+        h1 {
+          font-size: 1.5rem;
+          margin-bottom: 1rem;
+        }
+
+        .card-container {
+          margin-bottom: 20px;
+        }
+
+        .sidebar {
+          text-align: center;
+          margin: 20px;
+        }
+      `}</style>
     </div>
   );
 };
